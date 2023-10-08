fb565394f8ab34420685cf6353785eafe10d3ce9
Michael3573 <126564353+Michael3573@users.noreply.github.com>
2023-10-08 20:09:48 +0200

    commit-03

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,9 +1,8 @@
-import React from "react";
-import ContactListItem from "./ContactListItem";
+import React from 'react';
 
 const ContactList = ({ contacts, onDelete }) => (
   <ul className="contact-list">
-    {contacts.map((contact) => (
+    {contacts.map(contact => (
       <li className="phone-book" key={contact.id}>
         {contact.name}: {contact.number}
         <button className="del-btn" onClick={() => onDelete(contact.id)}>
